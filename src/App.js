30f9b21b4319da6621aaf6d8d0bbebc6374f2431
habbkad <habibkadiri52@gmail.com>
2022-11-12 15:30:53 +0000

    Crud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import { Row, Col } from "react-bootstrap";
+import Users from "./Components/Users";
+import UserForm from "./Components/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      students: [
+        {
+          name: "Philo",
+          gen: 60,
+          height: "2m",
+        },
+        {
+          name: "Sam Generals",
+          gen: 23,
+        },
+        {
+          name: "Miss Bentil",
+          gen: 23,
+        },
+        {
+          name: "Rabbi",
+          gen: 23,
+        },
+        {
+          name: "Joshua",
+          gen: 23,
+        },
+        {
+          name: "Hussein",
+          gen: 23,
+        },
+      ],
+    };
+  }
+
+  addStudent = (newStudent) => {
+    this.setState({
+      students: [...this.state.students, newStudent],
+    });
+  };
+  //functional Component
+  // const addStudent = (newStudent) => {
+  //  setStudents([...students,newStudent])
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <Container>
+          <Row>
+            <Col>
+              <UserForm addStudent={this.addStudent} />
+            </Col>
+            <Col>
+              <Users students={this.state.students} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
